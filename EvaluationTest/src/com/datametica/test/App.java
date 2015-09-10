518fb0f917f3d7da619bb4cfd319654e892bcb12
datametica <mohammedayaz1993@gmail.com>
2015-09-10 11:09:09 +0530

    test completed

--- EvaluationTest/src/com/datametica/test/App.java
+++ EvaluationTest/src/com/datametica/test/App.java
@@ -1,11 +1,32 @@
 package com.datametica.test;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("new test");
+		int i,j,k,m;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter number of lines to be printed");
+		i=sc.nextInt();
+		m=i-1;
+		for (int l = 0; l < i; l++) {
+
+			k=0;
+			j=m-l;
+		while(j>0) {
+			System.out.print("-");
+			j--;
+		}
+		while(k<=l){
+			System.out.print("*");
+			k++;
+		}
+		System.out.println(" ");
 	}
 
+}
 }
